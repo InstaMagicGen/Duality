9aff218ca3c25db8177ea454e93aafa6596fd41a
Soulset-navigator <zr.mehdi01@gmail.com>
2025-12-17 15:41:48 +0000

    Fix paths Header et translations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import Header from "./components/Header";
+import Header from "../components/Header";
 import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
